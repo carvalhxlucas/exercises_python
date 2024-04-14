3c16d8ae9784c7682a20ae232b2adc6036bc5819
Lucas Carvalho <carvalhxlucas@gmail.com>
2024-04-14 18:04:06 -0300

    resolution 18

--- estrutura_sequencial/estrutura_sequencial_18.py
+++ estrutura_sequencial/estrutura_sequencial_18.py
@@ -4,3 +4,11 @@
 
 ''' 18 - Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), 
 calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).'''
+
+tamanhoArquivo = float(input('Informe o tamanho do arquivo que deseja baixar: '))
+velocidadeInternet = int(input('Informe a velocidade da sua conexão em Mbps: '))
+
+tempoEstimado = (tamanhoArquivo / (velocidadeInternet / 8)) / 60
+
+
+print('Tempo estimado para a conclusão do download: ', tempoEstimado,' minutos.')
