fcfec64a4d0fe62cd40106e1962531a057e8a394
Lucas Carvalho <carvalhxlucas@gmail.com>
2024-04-15 16:03:10 -0300

    estrutura de decisao

---
+++ estrutura_descisao/estrutura_decisao_1.py
@@ -0,0 +1,13 @@
+# Lista de exercícios de Estrutura de Decisao
+
+# link da lista: https://wiki.python.org.br/EstruturaDeDecisao
+
+# 1 - Faça um Programa que peça dois números e imprima o maior deles.
+
+num1 = int(input('Informe um número: '))
+num2 = int(input('Informe outra número: '))
+
+if num1 > num2:
+    print('Este número é maior: ',num1)
+else:
+    print('Este número é maior: ',num2)
